ec76c8be95683ae61de8b96fe45bd855b4688518
katy.korotkih <katy_korotkih@mail.ru>
2022-12-27 17:47:34 +0300

    HW 7 added

---
+++ HW_007.py
@@ -0,0 +1,10 @@
+#дан список a = [4, 3, -10, 1, 7, 12], изменить его а=[4, -10, 12, 3, 1, 7]
+# a = [4, 3, -10, 1, 7, 12]
+# b = list(filter(lambda x: not x%2, a))
+# c = list(filter(lambda x: x%2, a))
+# res = b + c
+# print(res)
+
+# 1. напечатать сторку в одну линию - C:\WINDOWS\System32\drivers\etc\nst
+f = open('new_file.txt', 'r')
+print(f.readline())
